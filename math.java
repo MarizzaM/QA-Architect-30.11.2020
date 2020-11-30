f9c122e44e4a1e98eb96945be1ad0176c1f97311
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2020-11-30 18:59:02 +0200

    Create math.java

---
+++ math.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int y1 = -8;
+        y1 = y1 * 2/4*-1;
+
+        int z1 = 4;
+        z1 = y1*y1+y1+12;
+
+        System.out.println("y1= "+y1);
+        System.out.println("z1= "+z1);
+
+    }
+}
